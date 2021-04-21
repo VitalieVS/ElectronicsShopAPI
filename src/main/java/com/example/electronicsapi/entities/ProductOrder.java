2f3da1d2ee1f24a6a7d943024f6d4d8ecc9619eb
VitalieVSV <dut66AHJCT>
2021-04-21 20:51:36 +0300

    Size fixed

--- src/main/java/com/example/electronicsapi/entities/ProductOrder.java
+++ src/main/java/com/example/electronicsapi/entities/ProductOrder.java
@@ -1,18 +1,27 @@
 package com.example.electronicsapi.entities;
 
-import java.util.List;
-
 public class ProductOrder {
     private long id;
     private double price;
     private int quantity;
     private String color;
 
-    public ProductOrder(long id, double price, int quantity, String category, String color) {
+    private String size;
+
+    public ProductOrder(long id, double price, int quantity, String color, String size) {
         this.id = id;
         this.price = price;
         this.quantity = quantity;
         this.color = color;
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
     }
 
     public void setId(long id) {
@@ -26,7 +35,6 @@ public class ProductOrder {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
 
     public void setColor(String color) {
         this.color = color;
